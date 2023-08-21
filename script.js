f29037f617e4e6ef27c64ef0e7de9aa578380bbe
Marco Santini <marcosantinia12@gmail.com>
2023-08-20 20:55:30 -0700

    Update to UI, added modal and 'add book' functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function Book(title, author, pages, pagesRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.pagesRead = pagesRead;
+}
+
+Book.prototype.info = function(){
+    console.log(this.title + " by " + this.author + ", " + this.pages + " pages, " + this.status);
+}
+
+const addBookBtn = document.getElementById("add-book-btn");
+addBookBtn.addEventListener('click', function(){
+    console.log("button clicked");
+    const bookModal = document.querySelector('.book-modal');
+    const overlay = document.querySelector('.overlay');
+    bookModal.style.display = 'block';
+    overlay.style.display = 'block';
+
+});
+
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener('click', function() {
+    const book = new Book(title,author,pages,pagesRead);
+    book.info();    
+});
