6d88fc392987eeb156840a8374628bdcfd1ba466
Marco Santini <marcosantinia12@gmail.com>
2023-08-27 15:01:59 -0700

    Added edit book functionality

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const bookModal = document.querySelector('.book-modal');
+const overlay = document.querySelector('.overlay');
+let titleInput = document.getElementById('title-input');
+let authorInput = document.getElementById('author-input');
+let pagesInput = document.getElementById('pages-input');
+let pagesReadInput = document.getElementById('pages-read-input');
+let notesInput = document.getElementById('notes-textarea');
+const addBookBtn = document.getElementById('add-book-btn');
+const submitBtn = document.getElementById('submit-btn')
+const cancelBtn = document.getElementById('cancel-btn');
+const deleteBtn = document.getElementById('delete-btn');
+const bookCardContainer = document.querySelector('.book-card-container');
+
+let bookCardElements = []
+const myLibrary = [];
+let currentBook = "";
+let editingBook = false;
+
 
 function Book(title, author, pages, pagesRead, notes) {
     this.title = title;
@@ -6,55 +24,115 @@ function Book(title, author, pages, pagesRead, notes) {
     this.pagesRead = pagesRead;
     this.notes = notes
 }
-
 Book.prototype.info = function(){
     console.log(this.title + " by " + this.author + ", " + this.pages + " pages, " + this.pagesRead + " pages read");
 }
+function clearInputs(){
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    pagesInput.value = '';
+    pagesReadInput.value = '';
+    notesInput.value = '';
+}
+function addBookToLibrary() {
+    const book = new Book(titleInput.value,authorInput.value,pagesInput.value,pagesReadInput.value,notesInput.value);
+    book.info;
+    myLibrary.push(book);
+  
+    clearInputs();    
+}
+function updateDisplay(){
+    bookCardContainer.textContent = ''; //Clears out old display
+    bookCardElements = []; //Clears out old list
 
-const bookModal = document.querySelector('.book-modal');
-const overlay = document.querySelector('.overlay');
+    //Creates book card for each book in library 
+    for (let i = 0; i < myLibrary.length; i++) {
+        //create book card elements
+        const bookCard = document.createElement("div");
+            bookCard.classList.add('book-card');
+            bookCard.setAttribute('data-book-index', i);
+            bookCardContainer.appendChild(bookCard);
+        const titleOutput = document.createElement('p');
+            titleOutput.textContent = myLibrary[i].title;
+            bookCard.appendChild(titleOutput);
+        const authorOutput = document.createElement('p');
+            authorOutput.textContent = myLibrary[i].author;
+            bookCard.appendChild(authorOutput);
+        const pagesOutput = document.createElement('p');
+            pagesOutput.textContent = myLibrary[i].pages;
+            bookCard.appendChild(pagesOutput);
+        const progress = document.createElement('progress');
+            console.log(pagesReadInput.value, pagesInput.value);
+            progress.value = (myLibrary[i].pagesRead/myLibrary[i].pages)*100;
+            progress.max = 100;
+            bookCard.appendChild(progress);
 
-const addBookBtn = document.getElementById("add-book-btn");
-addBookBtn.addEventListener('click', function(){
-    console.log("button clicked");
-
+        // Store the book card element in the array
+        bookCardElements.push(bookCard);
+    }
+    editBook();
+}
+function openModal(){
     bookModal.style.display = 'flex';
     overlay.style.display = 'block';
-});
-
-const submitBtn = document.getElementById("submit-btn");
-submitBtn.addEventListener('click', function() {
-    const titleInput = document.getElementById('title-input').value;
-    const authorInput = document.getElementById('author-input').value;
-    const pagesInput = document.getElementById('pages-input').value;
-    const pagesReadInput = document.getElementById('pages-read-input').value;
-    const notesInput = document.getElementById('notes-textarea').value;
-
-    //Edit Existing Book
-
-
-    //Add New Book
-    const book = new Book(titleInput,authorInput,pagesInput,pagesReadInput, notesInput);
-    book.info();    
-    const bookCard = document.createElement("div");
-        bookCard.classList.add('book-card');
-        const bookCardContainer = document.querySelector('.book-card-container');
-        bookCardContainer.appendChild(bookCard);
-    const titleOutput = document.createElement('p');
-        titleOutput.textContent = titleInput;
-        bookCard.appendChild(titleOutput);
-    const authorOutput = document.createElement('p');
-        authorOutput.textContent = authorInput;
-        bookCard.appendChild(authorOutput);
-    const pagesOutput = document.createElement('p');
-        pagesOutput.textContent = pagesInput;
-        bookCard.appendChild(pagesOutput);
-    const progress = document.createElement('progress');
-        progress.value = (pagesReadInput/pagesInput)*100;
-        progress.max = 100;
-        bookCard.appendChild(progress);
-
+}
+function closeModal(){
     bookModal.style.display = 'none';
     overlay.style.display = 'none';
+}
+
+addBookBtn.addEventListener('click', function(){
+    console.log("Add book button clicked");
+    openModal();
+});
+
+function editBook(){
+    bookCardElements.forEach((bookCard, index) => {
+        bookCard.addEventListener('click', function() {
+            // Perform actions when the book card is clicked
+            console.log('Book card clicked:', myLibrary[index].title);
+            // You can access the book's properties using myLibrary[index]
+            titleInput.value = myLibrary[index].title
+            authorInput.value = myLibrary[index].author
+            pagesInput.value = myLibrary[index].pages
+            pagesReadInput.value = myLibrary[index].pagesRead
+            notesInput.value = myLibrary[index].notes
     
+            editingBook = true;
+            currentBook = myLibrary[index];
+
+            openModal();
+        });
+    });
+}
+
+submitBtn.addEventListener('click', function() {
+    console.log("Submit button clicked");
+
+    //Edit Existing Book
+    if(editingBook === true){ //Edit Book
+        console.log('editing book');
+        //need to target currently matching selected bookCard book
+        currentBook.title = titleInput.value;
+        currentBook.author = authorInput.value;
+        currentBook.pages = pagesInput.value;
+        currentBook.pagesRead = pagesReadInput.value;
+        currentBook.notes = notesInput.value;
+    }else{//Add New Book
+    addBookToLibrary();
+    }
+    console.log(myLibrary);
+    updateDisplay();
+    closeModal();
+});
+
+cancelBtn.addEventListener('click', function(){
+    editingBook = false;
+    closeModal();
+    clearInputs();    
+});
+deleteBtn.addEventListener('click', function(){
+    // editingBook = false;
+    alert('Are you sure you want to delete?');
 });
